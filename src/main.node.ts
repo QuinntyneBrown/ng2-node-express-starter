05f22a58f601fa0184908a191a519c84f14c6202
Quinntyne Brown <quinntynebrown@gmail.com>
2016-10-02 16:46:49 -0400

    server

--- src/main.node.ts
+++ src/main.node.ts
@@ -1,1 +1,22 @@
-﻿
+﻿import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { UniversalModule } from 'angular2-universal';
+
+import { AppComponent } from './app/app.component';
+import { LandingPageComponent } from "./app/pages/landing-page.component";
+
+@NgModule({
+    bootstrap: [AppComponent],
+    declarations: [AppComponent, LandingPageComponent],
+    imports: [
+        UniversalModule, // NodeModule, NodeHttpModule, and NodeJsonpModule are included
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', component: LandingPageComponent, pathMatch: 'full' }
+        ])
+    ]
+})
+export class MainModule {
+
+}
